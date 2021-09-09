0dcddba4ebc63bfb2083850b04d6e7340b5dcc7d
FabienBataille <fabienbataille.dev@gmail.com>
2021-09-09 10:50:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const { useState, useEffect } = React;
 
 function App() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible2, setIsVisible2] = useState(true);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const listenToScroll = () => {
+    let heightToHideFrom = 60;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    setHeight(winScroll);
+
+    if (winScroll < heightToHideFrom) {
+      isVisible && setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+
+    if (winScroll > heightToHideFrom) {
+      isVisible2 && setIsVisible2(true);
+    } else {
+      setIsVisible2(false);
+    }
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="height">
+        <b>height: {height} </b>
+      </div>
+      {isVisible && (
+        <div id="header-mastok">Content hidden when scrolled beyond 200px</div>
+      )}
+
+      {isVisible2 && (
+        <div id="header-small">Content header small</div>
+      )}
     </div>
   );
 }
